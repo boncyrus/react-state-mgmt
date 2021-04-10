a305c02de02dbb6068bdb088e0ae300446ac10fe
Bon Macalindong <bon@xam-consulting.com>
2021-04-11 00:19:21 +0800

    Change typing

--- src/store/observable/todo/epics.ts
+++ src/store/observable/todo/epics.ts
@@ -1,12 +1,12 @@
 import { switchMap, map, filter } from 'rxjs/operators';
-import { createAction, Action } from '@reduxjs/toolkit';
-import { from, Observable, of } from 'rxjs';
-import { getTodosCompleted, } from '../todo/todo'
+import { Action } from '@reduxjs/toolkit';
+import { from, Observable } from 'rxjs';
+import { getTodos, getTodosCompleted, } from '../todo/todo'
 import { Todo } from '../../../models/Todo';
 
 export const todoEpic = (actions$: Observable<Action>) => {
     return actions$.pipe(
-        filter(createAction('todo/getTodos').match),
+        filter(action => getTodos.match(action)),
         switchMap(() => {
             const getTodos = () => {
                 const request = fetch(`https://jsonplaceholder.typicode.com/todos`)
