06386a12f38ca7b34b6444773b1fc1198bccaffe
Bon Macalindong <bon@xam-consulting.com>
2021-04-10 23:51:20 +0800

    minor refactoring

--- src/store/observable/rootStore.ts
+++ src/store/observable/rootStore.ts
@@ -1,18 +1,20 @@
 import { RootState } from './../rootState';
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import { todoEpic } from './todo/epics';
-import { configureStore } from '@reduxjs/toolkit';
-import TodoReducer from './todo/todo';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import TodoSlice from './todo/todo';
 
 const epicMiddleware = createEpicMiddleware();
 
 const rootEpic = combineEpics(todoEpic);
 
+const reducer = combineReducers<RootState>({
+    todo: TodoSlice
+})
+
 export function initStore() {
     const store = configureStore<RootState>({
-        reducer: {
-            todo: TodoReducer
-        },
+        reducer,
         middleware: [epicMiddleware]
     });
 
